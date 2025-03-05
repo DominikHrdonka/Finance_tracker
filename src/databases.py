5262f3b141e3f353dee4afde9ebff154b5ca2c52
Dominik Hrdonka <hrdonka.d@gmail.com>
2025-03-05 14:18:57 +0100

    add comments in databases.py

--- src/databases.py
+++ src/databases.py
@@ -1,9 +1,15 @@
 import sqlite3
 
-#Creating database for inserted revenues and expenses
+#Creating database for the whole project
 
+"""
+NOTE:
+- need to figure out the path to the DB - it will be different on every machine
+"""
 with sqlite3.connect('finance_tracker_database.db') as Connection:
     cursor = Connection.cursor()
+
+    #Creating table for revenues and expanses
 
     cursor.execute(
         'CREATE TABLE IF NOT EXIST revenues_expenses (id INTEGER PRIMARY KEY, revenue BOOLEAN, date TEXT);'
